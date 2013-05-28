f5b88592cc6b52620a35e9c9f14df0eed7601f84
Uncrowned <link_92@list.ru>
2013-05-28 15:03:27 +0700

    now main.php contain programm code

--- main.php
+++ main.php
@@ -1,24 +1,133 @@
+<?php
+use Entities\Domain,
+  Entities\Words,
+  Entities\Region,
+  Entities\YP,
+  Entities\Frequency;
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link href="style.css" type="text/css" rel="stylesheet">
-        <title></title>
-    </head>
-    <body>
-        <div id="mainWrapper">
-            <form name="mainForm" id="mainForm" action="index.php?action=getstat" method="post">
-                <textarea name="words"></textarea>
-                <br>
-                <input name="url" placeholder="Input url..."/>
-                <select name="region">
-                    <option selected value="213">Москва</option>
-                    <option value="55">Омск</option>
-                </select>
-                <button name="submit">Send</button>
-            </form>
-        </div>
-    </body>
-</html>
+require_once("MegaIndex.php");
+require_once("MegaIndexDb.php");
 
+try
+{
+  if (!empty($_POST['words']) && !empty ($_POST['url']) && !empty ($_POST['region']))
+  {
+    $time = 3600*24*5;
+    $time = time()-$time;
+    $db = new MegaIndexDb();
+    $db->createConnection('megaindex', 'pdo_mysql', 'localhost', 'root', 'root');
+
+    $em = $db->getEntityManager();
+
+    $regionCode = (int)$_POST['region'];
+    $url = trim($_POST['url']);
+    $requests = explode(',', $_POST['words']);
+
+    $api = new MegaIndex($url, 'sagdiv@gmail.com', 'VqGPOv');
+
+    $domain = $em->getRepository('Entities\Domain')->findBy(array('name' => $url));
+    if (empty($domain))
+    {
+      $domain[0] = new Domain();
+      $domain[0]->setName($url);
+      $em->persist($domain[0]);
+
+      $em->flush();
+    }
+
+    $region = $em->getRepository('Entities\Region')->findBy(array('code' => $regionCode));
+
+    foreach ($requests as $request)
+    {
+      $request = trim($request);
+
+      $word = $em->createQuery("select words from Entities\Words words where words.name=:name and words.date>=".$time);
+      $word = $word->setParameter('name', $request)->getResult();
+      if (empty($word))
+      {
+        $word = $em->createQuery("select words from Entities\Words words where words.name=:name");
+        $word = $word->setParameter('name', $request)->getResult();
+        if (!empty($word[0]))
+        {
+          $word[0]->setName($request);
+          $word[0]->setPrice($api->getPrice($request));
+          $word[0]->setDate(time());
+        }
+        else
+        {
+          $word[0] = new Words();
+          $word[0]->setName($request);
+          $word[0]->setPrice($api->getPrice($request));
+          $word[0]->setDate(time());
+        }
+        $em->persist($word[0]);
+
+        $em->flush();
+      }
+
+      $yp = $em->createQuery("select yp from Entities\YP yp where yp.domain=".$domain[0]->getId()." and yp.region=".$region[0]->getId()." and yp.word=".$word[0]->getId()." and yp.date>=".$time)->getResult();
+      if (empty($yp))
+      {
+        $yp = $em->createQuery("select yp from Entities\YP yp where yp.domain=".$domain[0]->getId()." and yp.region=".$region[0]->getId()." and yp.word=".$word[0]->getId())->getResult();
+        if (!empty($yp[0]))
+        {
+          $yp[0]->setDate(time());
+          $yp[0]->setRegion($region[0]);
+          $yp[0]->setWord($word[0]);
+          $yp[0]->setDomain($domain[0]);
+          $yp[0]->setPosition($api->getYandexPosition($word[0]->getName(), $region[0]->getCode()));
+        }
+        else
+        {
+          $yp[0] = new YP();
+          $yp[0]->setDate(time());
+          $yp[0]->setRegion($region[0]);
+          $yp[0]->setWord($word[0]);
+          $yp[0]->setDomain($domain[0]);
+          $yp[0]->setPosition($api->getYandexPosition($word[0]->getName(), $region[0]->getCode()));
+        }
+        $em->persist($yp[0]);
+
+        $em->flush();
+      }
+
+      $freq = $em->createQuery("select freq from Entities\Frequency freq where freq.region=".$region[0]->getId()." and freq.word=".$word[0]->getId()." and freq.date>=".$time)->getResult();
+      if (empty($freq))
+      {
+        $freq = $em->createQuery("select freq from Entities\Frequency freq where freq.region=".$region[0]->getId()." and freq.word=".$word[0]->getId())->getResult();
+        if (!empty($freq[0]))
+        {
+          $freq[0]->setWord($word[0]);
+          $freq[0]->setRegion($region[0]);
+          $freq[0]->setDate(time());
+          $freq[0]->setFrequency($api->getWordStat($word[0]->getName(), $region[0]->getCode()));
+        }
+        else
+        {
+          $freq[0] = new Frequency();
+          $freq[0]->setWord($word[0]);
+          $freq[0]->setRegion($region[0]);
+          $freq[0]->setDate(time());
+          $freq[0]->setFrequency($api->getWordStat($word[0]->getName(), $region[0]->getCode()));
+        }
+        $em->persist($freq[0]);
+
+        $em->flush();
+      }
+      //this arrays contain answers on request
+      $domains[] = $domain[0];
+      $words[] = $word[0];
+      $regions[] = $region[0];
+      $yps[] = $yp[0];
+      $freqs[] = $freq[0];
+    }
+  }
+  else
+  {
+    //include_once("error.php");
+  }
+}
+catch (Exception $e)
+{
+  echo $e->getMessage();
+}
