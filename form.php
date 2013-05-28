238adaa83d491240f6cb33b970593bb693185081
Uncrowned <link_92@list.ru>
2013-05-28 15:05:35 +0700

    form.php contain form for application

---
+++ form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <link href="style.css" type="text/css" rel="stylesheet">
+  <title></title>
+</head>
+<body>
+  <div id="mainWrapper">
+    <form name="mainForm" id="mainForm" action="index.php?action=getstat" method="post">
+      <textarea name="words"></textarea>
+      <br>
+      <input name="url" placeholder="Input url..."/>
+      <select name="region">
+        <option selected value="213">Москва</option>
+        <option value="55">Омск</option>
+      </select>
+      <button name="submit">Send</button>
+    </form>
+  </div>
+</body>
+</html>
